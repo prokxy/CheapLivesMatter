8fb519767cdae2d18e75de6c2af56fcde1064bd4
prokxy <99291552+prokxy@users.noreply.github.com>
2022-11-05 20:13:30 +0100

    initial
    upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var mainMenu = document.getElementById("mainMenu");
+    var menuToggle = document.getElementById("menu-toggle");
+
+    mainMenu.onclick = function(){
+        if (menuToggle.checked == true){
+            menuToggle.checked = false;
+        }
+        else{
+            menuToggle.checked = true;
+        }
+}
